59a388b4b281ad8b789f62ddfe4c351a08033d64
Sneha Patel <snehapatel@Snehas-MBP.lan>
2022-06-27 19:07:00 -0400

    boiler-plate website structure and  about page

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+
+
+/* Run server */
+
+console.log('Author: Sneha Patel (snehagpatel)');
+http.listen(8080);
+console.log('Server running on port 8080. Now open http://localhost:8080/ in your browser!');
